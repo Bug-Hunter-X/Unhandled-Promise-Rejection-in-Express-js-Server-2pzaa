d04df0c153ec51f75611b153f24f5ebd2ed2e30d
BugHunter-X <askzenai@gmail.com>
2025-01-23 11:50:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  // Simulate an asynchronous operation that might throw an error
+  setTimeout(() => {
+    const randomNumber = Math.random();
+    if (randomNumber < 0.5) {
+      // Simulate an unexpected error
+      throw new Error('Something went wrong!');
+    } else {
+      res.send('Hello from Express!');
+    }
+  }, 1000); 
+});
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
